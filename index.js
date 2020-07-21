e843342de559ec08069057921b0587eec8deecc1
Joshua Martin <the.manuscriptmaster@gmail.com>
2020-07-21 15:37:46 -0400

    Move ramda deps to dev deps only

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-import both from 'ramda/src/both.js';
-import curry from 'ramda/src/curry.js';
-import has from 'ramda/src/has.js';
-import partialRight from 'ramda/src/partialRight.js';
-
-const chainRec = curry(async (fn, acc) => {
+const chainRec = async (fn, acc) => {
   const next = value => ({ tag: next, value });
   const done = value => ({ tag: done, value });
   const { value, tag } = await fn(next, done, acc);
   return tag === next ? chainRec(fn, value) : value;
-});
+};
 
-const _serial = curry((fn, acc) => chainRec(
+const _serial = (fn, acc) => chainRec(
   (next, done, { data, page, limit = 0 }) => fn(page).then(d =>
     d.length === 0 || d.length < limit
       ? done(data.concat(d))
       : next({ data: data.concat(d), page: page + 1, limit: d.length })
   ),
   acc
-));
+);
 
-const _concurrent = curry((fn, acc) => chainRec(
+const _concurrent = (fn, acc) => chainRec(
   (next, done, { data, page, limit, total }) =>
     total === undefined
       ? fn(page).then(({ data: d, total: t }) =>
@@ -32,25 +27,28 @@ const _concurrent = curry((fn, acc) => chainRec(
         : done(Promise.all([...data, fn(page).then(res => res.data)]).then(arr => arr.flat()))
   ,
   acc
-));
+);
 
 const isValidCursor = cursor => ((typeof cursor === 'string' && cursor.length > 0) || typeof cursor === 'number');
 
-const _cursor = curry((fn, acc) => chainRec(
+const _cursor = (fn, acc) => chainRec(
   (next, done, { data, cursor }) => fn(cursor).then(({ data: d, cursor: c }) =>
     d.length > 0 && isValidCursor(c)
       ? next({ data: data.concat(d), cursor: c })
       : done(data.concat(d))
   ),
   acc
-));
+);
 
-export const serial = partialRight(_serial, [{ data: [], page: 1 }]);
-export const concurrent = partialRight(_concurrent, [{ data: [], page: 1 }]);
-export const cursor = partialRight(_cursor, [{ data: [] }]);
+export const serial = fn => _serial(fn, { data: [], page: 1 });
+export const concurrent = fn => _concurrent(fn, { data: [], page: 1 });
+export const cursor = fn => _cursor(fn, { data: [] });
 
 const raise = err => { throw err };
 const p = fn => async (...args) => fn(...args);
+
+const isConcurrentObject = obj => typeof obj === 'object' && obj.hasOwnProperty('data') && obj.hasOwnProperty('total');
+const isCursorObject = obj => typeof obj === 'object' && obj.hasOwnProperty('data') && obj.hasOwnProperty('cursor');;
 
 /**
 * Executes a paginated function over all pages, preferring concurrency where possible.
@@ -66,9 +64,9 @@ const p = fn => async (...args) => fn(...args);
 export default fn => p(fn)().then(d =>
   Array.isArray(d) ?
     d.length ? _serial(p(fn), { data: d, page: 2 }) : d
-: both(has('data'), has('total'))(d) ?
+: isConcurrentObject(d) ?
     d.data.length ? _concurrent(p(fn), { ...d, page: 2, limit: d.data.length }) : d.data
-: both(has('data'), has('cursor'))(d) ?
+: isCursorObject(d) ?
     d.data.length && isValidCursor(d.cursor) ? _cursor(p(fn), d) : d.data
 :
     raise(new TypeError('Function must return an array or an object with an array'))
